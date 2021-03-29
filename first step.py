ee15bdb045ec24f108daa731dd4688cf77b34cb1
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-03-29 20:35:22 +0300

    Update first step.py

--- first step.py
+++ first step.py
@@ -1,56 +1,30 @@
 import random
 
 
-def welcome():
-    print('1. Create an account')
-    print('2. Log into account')
-    print('0. Exit')
-    answer = input()
-    return answer
-
-
-answer = welcome()
-account = []
-
-
 class Account:
+    all_accounts = []
+    account = []
+    balance = 0
 
     def __init__(self, card_number, pin):
         self.card_number = card_number
         self.pin = pin
         self.balance = 0
+        Account.all_accounts.append(self)
 
 
-Account.card_number = 0
-Account.pin = 0
-Account.balance = 0
-all_account = []
-
-
-def create_account():
+def create():
     Account.card_number = int('400000' + str(int(random.random() * 1000000000)))
     Account.pin = random.randint(1111, 9999)
-    account = [Account.card_number, Account.pin, Account.balance]
-    all_account.append(account)
-    print(all_account)
-    return all_account, account
+    Account.account = [Account.card_number, Account.pin, Account.balance]
+    Account.all_accounts.append(Account.account)
 
 
-def look_account():
-    print('Enter your card number:')
-    card_number = input()
-    print('Enter your PIN:')
-    pin = input()
-    for account in all_account:
-        if [card_number, pin] in all_account:
-            print(account)
-        else:
-            print('переписывай')
+n = 0
+while n < 10:
+    create()
+    n += 1
+print(Account.all_accounts)
 
-
-def exit():
-    print('Bye!')
-
-
-create_account()
-look_account()
+for Account.account in Account.all_accounts:
+    print(str(Account.account[0]) + ' = ' + str(Account.account[2]))
