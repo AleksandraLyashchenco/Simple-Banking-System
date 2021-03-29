391d9441e61a89c8b8881de98cf9ff4e23929b6a
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-03-29 22:42:48 +0300

    Update first step.py

--- first step.py
+++ first step.py
@@ -3,7 +3,7 @@ import random #импорт модуля "рандом"
 
 class Account: # обьявляю класс (имя - "Аккаунт")
     all_accounts = []# обьявляю переменную, присваиваю значение - пустой список (атрибут)
-    balance = 0# обьявляю переменную, присваиваю значение - 0, (атрибут)
+    
 
     def __init__(self, card_number, pin): #конструктор инит
         self.card_number = card_number # личному "card_number" присваиваю значение "card_number"
@@ -14,7 +14,7 @@ class Account: # обьявляю класс (имя - "Аккаунт")
 def create():#обьявляю метод 
     Account.card_number = int('400000' + str(int(random.randint(1000000000, 9999999999))))#присваиваю значение атрибуту card_number
     Account.pin = random.randint(1111, 9999)#присваиваю значение атрибуту pin
-    Account.account = [Account.card_number, Account.pin, Account.balance]#обьявляю переменную и присваивааю ей значение
+    account = Account(Account.card_number, Account.pin)
     Account.all_accounts.append(Account.account)#добавляю список account в список all_account
 
 
@@ -25,4 +25,4 @@ while n < 10:# "пока n меньше 10":
 print(Account.all_accounts)#печатать all_accounts
 
 for Account.account in Account.all_accounts:#для каждого елемента account в списке all_accounts:
-    print(str(Account.account[0]) + ' = ' + str(Account.account[2]))# печатать " первый элемент = третий элемент"
+    print(str(Account.account.card_number) + ' = ' + str(Account.account.pin))# печатать " первый элемент = третий элемент"
