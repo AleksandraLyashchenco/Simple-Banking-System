98c35256c892e530b549e3ee42b7d21638a3e8a3
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-03-29 22:59:31 +0300

    Update first step.py

--- first step.py
+++ first step.py
@@ -1,28 +1,28 @@
-import random #импорт модуля "рандом"
+import random 
 
 
-class Account: # обьявляю класс (имя - "Аккаунт")
-    all_accounts = []# обьявляю переменную, присваиваю значение - пустой список (атрибут)
+class Account: 
+    all_accounts = []
     
 
-    def __init__(self, card_number, pin): #конструктор инит
-        self.card_number = card_number # личному "card_number" присваиваю значение "card_number"
-        self.pin = pin#личному "card_number" присваиваю значение "card_number"
-        self.balance = 0#личному "pin" присваиваю значение "pin"
-     
+    def __init__(self, card_number, pin): 
+        self.card_number = card_number 
+        self.pin = pin
+        self.balance = 0
+        Account.all_accounts.append(account)
 
-def create():#обьявляю метод 
-    card_number = int('400000' + str(int(random.randint(1000000000, 9999999999))))#присваиваю значение атрибуту card_number
-    pin = random.randint(1111, 9999)#присваиваю значение атрибуту pin
+def create():
+    card_number = int('400000' + str(int(random.randint(1000000000, 9999999999))))
+    pin = random.randint(1111, 9999)
     account = Account(card_number, pin)
-    Account.all_accounts.append(account)#добавляю список account в список all_account
+    
 
 
-n = 0# переменная n, значение 0
-while n < 10:# "пока n меньше 10":
-    create()# вызывать функцию  create
-    n += 1# шаг 1
-print(Account.all_accounts)#печатать all_accounts
+n = 0
+while n < 10:
+    create()
+    n += 1
+print(Account.all_accounts)
 
-for account in Account.all_accounts:#для каждого елемента account в списке all_accounts:
-    print(str(account.card_number) + ' = ' + str(account.pin))# печатать " первый элемент = третий элемент"
+for account in Account.all_accounts:
+    print(str(account.card_number) + ' = ' + str(account.pin))
