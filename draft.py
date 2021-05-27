10ecdfd9413b5876db05a7f74594685d34e41f43
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-05-27 11:54:14 +0300

    Add files via upload

---
+++ draft.py
@@ -0,0 +1,30 @@
+def sum_sum(a, b):
+    return a + b
+
+
+def mul(a, b):
+    return summ(a, b) * b
+
+
+def sub(a, b):
+    return mul(a, b) // b
+
+
+def pow_pow(a, b):
+    return sub(a, b) ** b
+
+
+def to_upper_case(c):
+    return c.upper()
+
+
+def to_lower_case(d):
+    return d.lower()
+
+
+print(sum_sum(2, 3))
+print(mul(2,3))
+print(sub(2, 7))
+print(pow_pow(2, 5))
+print(to_upper_case('PpPpPpPp'))
+print(to_lower_case('SsSsSsSSS'))
