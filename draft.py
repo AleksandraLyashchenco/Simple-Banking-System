6e0bd681f50ee2a4ae88fd4104409111e1d3714a
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-05-28 18:32:01 +0300

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,16 +1,16 @@
 x = 5
-y = 3
+y = 10
 
 
-def sum_sum (x, y):
-    return x + y
+def sum_sum (a, b):
+    return a + b
 
 
-def mul(x, y):
+def mul(a, b):
     c = 1
     d = x
-    while c < y:
-        d = sum_sum(d, x)
+    while c < b:
+        d = sum_sum(d, a)
         c = sum_sum(c, 1)
     return d
 
