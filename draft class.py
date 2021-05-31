d599706413460f46a8b63b8cc994f5a14d209be6
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-05-31 20:44:20 +0300

    Update draft class.py

--- draft class.py
+++ draft class.py
@@ -6,22 +6,23 @@ class Account:
         self.pin = pin
         self.balance = 0
 
-    def income(self):
-        print('Введите сумму пополнения')
-        income = int(input())
-        self.balance = self.balance + income
-        print(self.balance)
-        return self.balance
+
 
     def output(self):
         print('Введите сумму перевода')
         transfer = int(input())
         self.balance = self.balance - transfer
         print(self.balance)
-        return  self.balance
+        return  self.balance, transfer
+
+    def income(self, transfer):
+        income = transfer
+        self.balance = self.balance + income
+        print(self.balance)
+        return self.balance
 
 first = Account(123456, 1234)
 second = Account(654321, 7654)
 
-first.income()
-first.income()
+
+
