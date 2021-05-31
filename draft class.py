2ed651182c0f26b228cddf188b2844f3d73eff7b
Aleksandra <73220615+AleksandraLyashchenco@users.noreply.github.com>
2021-05-31 20:49:41 +0300

    Update draft class.py

--- draft class.py
+++ draft class.py
@@ -9,16 +9,12 @@ class Account:
 
 
     def output(self):
-        print('Введите сумму перевода')
-        transfer = int(input())
         self.balance = self.balance - transfer
-        print(self.balance)
         return  self.balance, transfer
 
     def income(self, transfer):
         income = transfer
         self.balance = self.balance + income
-        print(self.balance)
         return self.balance
 
 first = Account(123456, 1234)
